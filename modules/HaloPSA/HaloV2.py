2ceafb436b5d05340784db7e3fcba8f6431fec64
BritishCopsAreClowns <finnryanit@gmail.com>
2024-03-26 12:29:38 +0000

    Updated haloV2

--- modules/HaloPSA/HaloV2.py
+++ modules/HaloPSA/HaloV2.py
@@ -1,11 +1,11 @@
 # Entirely re-written with classes
 
-
+# Apparently this is very, very bad
 
 # Modules to interact with Halo.
 # Some modules use specifc IDs, will try to clean this up as I go.
 
-import requests as rqst
+import requests
 import urllib.parse
 import json
 import os
@@ -17,112 +17,118 @@ HALO_SECRET = os.getenv('HALO_SECRET')
 HALO_API_URL = os.getenv('HALO_API_URL') 
 HALO_AUTH_URL = os.getenv('HALO_AUTH_URL')
 
+assetURL = HALO_API_URL+ '/asset/'
 
-class HaloPSA():
-    def __init__ (self,token):
-        header = { # Header with token
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + token
-            }
-        self.asset = self.assetC(self.header)
-    
-    class TEMPLATECLASS():
-        """ Asset actions """
-        def __init__(self):
-            self.ll = HALO_API_URL + ''
-            self.header = HaloPSA.header
-            
-        
-    def responseCode(data):
-        """ Work in Progress, halo code detection """
-        code = data.status_code
-        if code  in [200,201]:
-            return data.content
+
+# Headers
+
+def responseChecker(request,returnErrors=False):
+    """ Work in Progress, halo code detection """
+    code = request.status_code
+    # Success
+    if code in [200,201]:
+        if returnErrors==True:
+            return 'Success', request.content 
+        else:
+            return request.content 
+    # Add unique failures as found
+    else:
+        if returnErrors==True:
+            return 'Error',f'{code} - Other failure'
         else:
             return f'{code} - Other failure'
 
-    def getToken():
-        # Return auth token from Halo. 
-        authheader = { # Required by Halo, don't ask me why
-        'Content-Type': 'application/x-www-form-urlencoded'
-        }
-        payload = { # Create payload for Halo auth
-        'grant_type': 'client_credentials',
-        'client_id': HALO_CLIENT_ID,
-        'client_secret': HALO_SECRET,
-        'scope': 'all' 
-        }
-        request = rqst.post(HALO_AUTH_URL, headers=authheader, data=urllib.parse.urlencode(payload)) # Request auth token 
-        return json.loads(HaloPSA.responseCode(request))['access_token'] 
-    
-    class assetC():
-        """ Asset actions """
-        def __init__ (self):
-            self.asset = HALO_API_URL + '/asset/'
-            
-        def get(self,id):
-            """Get a single halo assets details."""
-            request = rqst.get(self.asset + str(id) +'?includedetails=True', headers = self.header)
-            return json.loads(HaloPSA.responseCode(request))
-        
-        def getAll(self):
-            """ Replaces Returns all Halo assets"""
-            request = rqst.get(self.asset, headers = self.header)
-            return json.loads(HaloPSA.responseCode(request))
-        
-        def search(self,query):
-            """ Search Halo assets 
-            DOES NOT DO ANYTHING RIGHT NOW """
-            pass
-        
-        def update(self,payload):
-            """ Update asset.  ID provided in Payload (for now.) 
-            Payload should be formatted with json.dumps, will move that bit in here eventually."""
-            request = rqst.post(self.asset, headers = self.header, data=payload)
-            return HaloPSA.responseCode(request)
-    class ticket():
-        def createHaloTicket(payload):
-            # Create ticket
-            request = rqst.post(HALO_API_URL+ '/tickets/', headers = self.header, data=payload)
-            return HaloPSA.responseCode(request)                
 
-
-def createHaloTicket(payload, token):
-    # Create ticket
-    headers = { # Header with token
-    'Content-Type': 'application/json',
-    'Authorization': 'Bearer ' + token
+def createToken():
+    # Return auth token from Halo. 
+    authheader = { # Required by Halo, don't ask me why
+    'Content-Type': 'application/x-www-form-urlencoded'
     }
-    attemptUpdate = rqst.post(HALO_API_URL+ '/tickets/', headers = headers, data=payload)
-    return attemptUpdate
+    payload = { # Create payload for Halo auth
+    'grant_type': 'client_credentials',
+    'client_id': HALO_CLIENT_ID,
+    'client_secret': HALO_SECRET,
+    'scope': 'all' 
+    }
+    
+    request = requests.post(HALO_AUTH_URL, headers=authheader, data=urllib.parse.urlencode(payload)) # Request auth token
+    response = responseChecker(request,True)
+    if response[0] is 'Success':
+        return response[1]['access_token']
+    else:
+        return response
 
 
-def searchHaloTicket(query, token):
-    """ Searches for a ticket """
-    headers = { # Header with token
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + token
-        }
-    request = rqst.get(HALO_API_URL+ '/tickets?' + urllib.parse.urlencode(query), headers = headers)
-    if request.status_code != 200:
-        return 'Failed to get tickets'
-    response = json.loads(request.content)
-    return response
 
-def manualTokenUpdate(key,token,id):
-    """ Manually update tokens for halo integrations.
+class asset():
+    """ Asset actions 
+    Initialize by running this once on its own, then run actions"""
+    def __init__(self):
+        token = createToken()
+        self.token = token
+        self.headerJSON = { # Header with token
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' +  token
+            }
+    
+    def get(self,id):
+        """Get a single halo asset's details.
+        ID = Asset ID"""
+        request = requests.get(assetURL + str(id) +'?includedetails=True', headers = self.headerJSON)
+        return responseChecker(request)
 
-    Make sure you have the integration type set to bearer token :)"""
-    headers = { # Header with token
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + token
-        }
-    payload = json.dumps([{
-        "new_client_secret": str(key),
-        "id": id 
-    }])
-    attemptUpdate = rqst.post(HALO_API_URL+ '/CustomIntegration', headers = headers, data=payload)
-    return attemptUpdate
+    
+    def getAll(self):
+        """ Replaces Returns all Halo assets"""
+        request = requests.get(assetURL, headers = self.headerJSON)
+        return responseChecker(request)
+    
+    def search(self,query):
+        """ Search Halo assets 
+        DOES NOT DO ANYTHING RIGHT NOW """
+        pass
+    
+    def update(self,payload):
+        """ Update asset.  ID provided in Payload (for now.) 
+        Payload should be formatted with json.dumps, will move that bit in here eventually."""
+        try:
+            payload = json.dumps(payload)
+        except:
+            # TODO Confirm it failed because its already JSON
+            pass
+        request = requests.post(self.asset, headers = self.header, data=payload)
+        return responseChecker(request)
+
+    
+    
+           
+class ticket():
+    def __init__(self):
+        token = createToken()
+        self.token = token
+        self.headerJSON = { # Header with token
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' +  token
+            }
+    def create(self, payload):
+        """ Create a ticket 
+        Payload must be formatted for now, will create a formatting tool later"""
+        try:
+            payload = json.dumps(payload)
+        except:
+            # TODO Confirm it failed because its already JSON
+            pass
+       
+        request = requests.post(HALO_API_URL+ '/tickets/', headers = self.header, data=payload)
+        return responseChecker(request)
+
+    def search(self,query, token):
+        """ Search ticket using Query (Later query will be its own thing so its easier to use) """
+        query = urllib.parse.urlencode(query)
+        request = requests.get(HALO_API_URL+ '/tickets?' + query, headers = self.headerJSON)
+        return responseChecker(request)
+
+
 
 
 
@@ -139,10 +145,10 @@ def productUpdate(token,updateField,originalText,replacementText):
         updateField: originalStr.replace(originalText,replacementText),
         "id": ID
         }])
-        attemptUpdate = rqst.post(HALO_API_URL+ '/item', headers = headers, data=payload)
+        attemptUpdate = requests.post(HALO_API_URL+ '/item', headers = headers, data=payload)
         return attemptUpdate.status_code
 
-    request = rqst.get(HALO_API_URL+ '/item', headers = headers)
+    request = requests.get(HALO_API_URL+ '/item', headers = headers)
     itemsList = json.loads(request.content)['items']
     for item in itemsList:
         if item == 'more': # Shows only 100 by default, this allows it to cycle through the remaining ones 
@@ -173,7 +179,7 @@ def userSearch(token,query):
         'Content-Type': 'application/json',
         'Authorization': 'Bearer ' + token
         }
-    request = rqst.get(HALO_API_URL+ '/users?' + urllib.parse.urlencode(query), headers = headers)
+    request = requests.get(HALO_API_URL+ '/users?' + urllib.parse.urlencode(query), headers = headers)
     if request.status_code != 200:
         return 'Failed to get users'
     response = json.loads(request.content)
@@ -183,7 +189,8 @@ def userSearch(token,query):
 
 
 def invoiceActivator(token,ids=None):
-    """ Activate invoices by ID(s). If no IDs sent, activate all invoices """
+    """ Set invoices to Active
+    If no IDs sent, all invoices will be set to Active """
     headers = { # Header with token
         'Content-Type': 'application/json',
         'Authorization': 'Bearer ' + token
@@ -191,7 +198,7 @@ def invoiceActivator(token,ids=None):
     query = {
         'type': 54,
     }
-    request = rqst.get(HALO_API_URL+ '/Template', headers = headers,params = query)
+    request = requests.get(HALO_API_URL+ '/Template', headers = headers,params = query)
     if request.status_code != 200:
         return 'Failed to get Templates'
     response = json.loads(request.content)
@@ -206,7 +213,7 @@ def invoiceActivator(token,ids=None):
                 'id':invoice['id']
                 
             }])
-            updateAttempt = rqst.post(HALO_API_URL+ '/Template', headers = headers,data = data)
+            updateAttempt = requests.post(HALO_API_URL+ '/Template', headers = headers,data = data)
             if updateAttempt.status_code !=[200,201]:
                 print('Failed')
             else:
@@ -214,3 +221,17 @@ def invoiceActivator(token,ids=None):
     return response
 
 
+def manualTokenUpdate(key,token,id):
+    """ Manually update tokens for halo integrations.
+
+    Make sure you have the integration type set to bearer token :)"""
+    headers = { # Header with token
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token
+        }
+    payload = json.dumps([{
+        "new_client_secret": str(key),
+        "id": id 
+    }])
+    attemptUpdate = requests.post(HALO_API_URL+ '/CustomIntegration', headers = headers, data=payload)
+    return attemptUpdate
